f47251f7b4462f0de3ba08b4a53e541c60fb70a7
bengsky <iqbal1bahari@gmail.com>
2016-06-02 11:13:07 +0700

    tampilan contact

--- resources/views/homepage/contact.blade.php
+++ resources/views/homepage/contact.blade.php
@@ -7,7 +7,21 @@
           <h1>Contact Us</h1>
           <p>
               Your Kargo User's Landing Page.
-          </p>  
+
+          </p>
+          <div class="row">
+            <div class="col-md-5">
+              <h2></h2>
+                  <img src="image/LOGODBK.jpg" alt="image" style="width:304px;height:228px;">
+              <p>
+                  Jl. S. Supriadi No.62, Bendogerit, Sananwetan,
+                 <br> Kota Blitar, Jawa Timur
+              </p>
+            </div>
+            <div class="col-md-7">
+
+            </div>
+          </div>
         </div>
     </div>
 </div>
