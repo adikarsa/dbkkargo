9c8be84d297742c1e2934b80e78a5b339c3e083b
fadlifzain <fadlifzain@gmail.com>
2016-06-08 11:27:47 +0700

    adding maps to contact + fixing homepage interface bug

--- resources/views/homepage/contact.blade.php
+++ resources/views/homepage/contact.blade.php
@@ -27,7 +27,22 @@
               </p>
             </div>
             <div class="col-md-7">
-
+              <div style="width:600px; overflow:hidden; height:400px; max-width:100%;">
+                <div id="embed-map-display" style="height:100%; width:100%;max-width:100%;">
+                  <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=-7.045016,+110.326627&key=AIzaSyAN0om9mFmy1QN6Wf54tXAowK4eT0ZUPrU">
+                  </iframe>
+                </div>
+                <a class="code-for-google-map" href="https://www.hostingreviews.website/compare/fatcow-vs-dreamhost" id="enable-maps-data">dreamhost fatcow</a>
+                <style>
+                  #embed-map-display
+                  .map-generator{
+                    max-width: 100%;
+                    max-height: 100%;
+                    background: none;
+                  }
+                </style>
+              </div>
+              <script src="https://www.hostingreviews.website/google-maps-authorization.js?id=cb4a36ac-877d-a19e-bfb5-5e64e44ef41c&c=code-for-google-map&u=1465363184" defer="defer" async="async"></script>
             </div>
           </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,10 +33,10 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="{{ Request::is( '/') ? 'active' : '' }}"><a href="{{ url('/') }}">Home</a></li>
-        <li class="{{ Request::is( 'services') ? 'active' : '' }}"><a href="{{ url('/services') }}">Services</a></li>
-        <li class="{{ Request::is( 'about') ? 'active' : '' }}"><a href="{{ url('/about') }}">About</a></li>
-        <li class="{{ Request::is( 'contact') ? 'active' : '' }}"><a href="{{ url('/contact') }}">Contact</a></li>
+        <li class="{{ Request::is('/') ? 'active' : '' }}"><a href="{{ url('/') }}">Home</a></li>
+        <li class="{{ Request::is('services') ? 'active' : '' }}"><a href="{{ url('/services') }}">Services</a></li>
+        <li class="{{ Request::is('about') ? 'active' : '' }}"><a href="{{ url('/about') }}">About</a></li>
+        <li class="{{ Request::is('contact') ? 'active' : '' }}"><a href="{{ url('/contact') }}">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         @if (Auth::check())
@@ -48,7 +48,7 @@
     </div>
   </div>
 </nav>
-<div class="content">
+<div class="{{ Request::is('/') ? '' : 'content' }}">
 
     @yield('content')
 
