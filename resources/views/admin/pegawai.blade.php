1f876056cc8c109b49db0f155b68a0b9ff6ebc20
fadlifzain <fadlifzain@student.telkomuniversity.ac.id>
2016-08-08 19:25:09 +0700

    8-8-15

--- resources/views/admin/pegawai.blade.php
+++ resources/views/admin/pegawai.blade.php
@@ -118,10 +118,77 @@
                     {{$user->contact}}
                   </td>
                   <td class="text-left" style="width:15%">
-                      <form action="" method="post" class="col-md-4 col-md-offset-1">
-                        <button type="submit" class="btn btn-xs glyphicon glyphicon-pencil">
-                        </button>
-                      </form>
+                      <div class="col-md-4 col-md-offset-1">
+                          <a class="btn btn-xs btn-primary glyphicon glyphicon-pencil" href="#{{$index+1}}" data-toggle="modal"></a>
+                      </div>
+                      <div id="{{$index+1}}" class="modal fade" role="dialog">
+                        <div class="modal-dialog">
+                          <!-- Modal content-->
+                          <div class="modal-content">
+                            <form class="form-horizontal" action="{{url('admin/editpegawai')}}" method="post">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Edit Pegawai</h4>
+                              </div>
+                              <div class="modal-body">
+
+                                <input type="hidden" id="e_id" name="e_id" value="{{ $user->id }}">
+
+                                <div class="form-group{{ $errors->has('e_name') ? ' has-error' : '' }}">
+                                    <label for="e_name" class="col-md-3 control-label">Nama</label>
+
+                                    <div class="col-md-7">
+                                        <input id="e_name" type="text" class="form-control" name="e_name" value="{{ $user->name }}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('e_email') ? ' has-error' : '' }}">
+                                    <label for="e_email" class="col-md-3 control-label">E-Mail</label>
+
+                                    <div class="col-md-7">
+                                        <input id="e_email" type="email" class="form-control" name="e_email" value="{{ $user->email }}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('e_role') ? ' has-error' : '' }}">
+                                    <label for="e_role" class="col-md-3 control-label">Role</label>
+
+                                    <div class="col-md-3">
+                                        <label class="control-label radio-inline"><input id="e_role" type="radio" class="btn btn-lg" name="e_role" value="admin">Admin</label>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label class="control-label radio-inline"><input id="e_role" type="radio" class="btn btn-lg" name="e_role" value="pegawai">Pegawai</label>
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('e_contact') ? ' has-error' : '' }}">
+                                    <label for="e_contact" class="col-md-3 control-label">Kontak</label>
+
+                                    <div class="col-md-7">
+                                        <input id="e_contact" type="text" class="form-control" name="e_contact" value="{{ $user->contact }}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('e_password') ? ' has-error' : '' }}">
+                                    <label for="e_password" class="col-md-3 control-label">Kata Kunci</label>
+
+                                    <div class="col-md-7">
+                                        <input id="e_password" type="password" class="form-control" name="password">
+                                    </div>
+                                </div>
+                              </div>
+
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-user"></i> Update
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+
                       <form action="{{url('admin/dltpegawai/'.$user->id)}}" method="post" class="col-md-4">
                         {{ method_field('DELETE') }}
                         <button type="submit" class="btn btn-xs btn-danger glyphicon glyphicon-trash">
